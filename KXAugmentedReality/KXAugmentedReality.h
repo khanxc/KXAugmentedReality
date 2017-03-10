ae1860e516f577f4bc861c52d7577ce47c04011c
khanxc <khan.kmk@gmail.com>
2017-03-10 16:58:08 +0530

    Initial Commit

---
+++ KXAugmentedReality/KXAugmentedReality.h
@@ -0,0 +1,19 @@
+//
+//  KXAugmentedReality.h
+//  KXAugmentedReality
+//
+//  Created by khan on 10/03/17.
+//  Copyright © 2017 Appyte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KXAugmentedReality.
+FOUNDATION_EXPORT double KXAugmentedRealityVersionNumber;
+
+//! Project version string for KXAugmentedReality.
+FOUNDATION_EXPORT const unsigned char KXAugmentedRealityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KXAugmentedReality/PublicHeader.h>
+
+
